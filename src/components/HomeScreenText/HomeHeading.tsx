a6d04c4c0dbd8f773419a8f02d23933fc0b92f04
wasswaenockmale <enockmale97@gmail.com>
2023-10-31 18:25:19 +0300

    Display text

---
+++ src/components/HomeScreenText/HomeHeading.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { FONTSIZE, COLOR } from '../../constants/contants'
+
+interface TextProps{
+  title: string
+}
+const HomeHeading = (
+  {
+    title
+  }:
+    TextProps) => {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text
+        style={[
+          styles.text,
+          {
+            fontFamily: "RalewaySemiBold",
+          }
+        ]}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+}
+
+export default HomeHeading
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal:10
+  },
+  text: {
+    fontSize: FONTSIZE.TITLE_1,
+    marginVertical: 5,
+    color: COLOR.ORANGE_300,
+  }
+})
