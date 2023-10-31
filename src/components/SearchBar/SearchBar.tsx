c265b8ea2d7b169e23488e0fd8c5fd1b7059fdd2
wasswaenockmale <enockmale97@gmail.com>
2023-10-31 18:29:26 +0300

    implemented the search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,17 +1,40 @@
 import React from 'react'
 import { StyleSheet, Text, TextInput, View } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, Entypo } from '@expo/vector-icons';
 import { COLOR } from '../../constants/contants';
 
-const SearchBar = () => {
+interface SearchBarProps{
+  searchWord: string,
+  handleSearchInput: (text:string) => void,
+  handleCancelClick: () => void
+}
+const SearchBar = (
+  {
+    searchWord,
+    handleCancelClick,
+    handleSearchInput
+  }: SearchBarProps
+) => {
   return (
     <View
       style={styles.container}
     >
-      <AntDesign name="search1" size={24} color={COLOR.B_100} />
+      <AntDesign name="search1" size={20} color={COLOR.B_100} />
       <TextInput
         placeholderTextColor={COLOR.B_100}
+        placeholder='search here...'
+        style={styles.searchInput}
+        onChangeText={handleSearchInput}
       />
+      {
+        searchWord &&
+        <Entypo
+          name="cross"
+          size={25}
+          color={COLOR.B_300}
+          onPress={handleCancelClick}
+        />
+      }
     </View>
   )
 }
@@ -21,11 +44,15 @@ export default SearchBar
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    alignItems: 'center',
     padding: 5,
+    borderRadius: 10,
     backgroundColor: COLOR.GREY_50,
-    borderRadius:10
+    marginTop:10
   },
   searchInput: {
     flex: 1,
+    padding: 2,
+    paddingHorizontal:5
   }
 })
