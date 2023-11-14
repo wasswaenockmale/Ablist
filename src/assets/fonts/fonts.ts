93305d25bc2a433756d3c6ec15f522ea20d296f6
wasswaenockmale <enockmale97@gmail.com>
2023-11-14 18:27:30 +0300

    fonts: added comfortaa files and raleway files

--- src/assets/fonts/fonts.ts
+++ src/assets/fonts/fonts.ts
@@ -1,50 +1,30 @@
 import { useFonts } from "expo-font";
 
-import {
-  Comfortaa_300Light,
-  Comfortaa_400Regular,
-  Comfortaa_500Medium,
-  Comfortaa_600SemiBold,
-  Comfortaa_700Bold,
-} from "@expo-google-fonts/comfortaa";
- 
-import {
-  Raleway_100Thin,
-  Raleway_200ExtraLight,
-  Raleway_300Light,
-  Raleway_400Regular,
-  Raleway_500Medium,
-  Raleway_600SemiBold,
-  Raleway_700Bold,
-  Raleway_800ExtraBold,
-  Raleway_900Black
-} from "@expo-google-fonts/raleway";
- 
-
 const fontNames = {
-  ComfortaaLight: Comfortaa_300Light,
-  Comfortaa_Regular: Comfortaa_400Regular,
-  ComfortaaMedium: Comfortaa_500Medium,
-  ComfortaaSemiBold: Comfortaa_600SemiBold,
-  ComfortaaBold: Comfortaa_700Bold,
-  RalewayThin: Raleway_100Thin,
-  RalewayExtraLight: Raleway_200ExtraLight,
-  RalewayLight: Raleway_300Light,
-  RalewayRegular: Raleway_400Regular,
-  RalewayMedium: Raleway_500Medium,
-  RalewaySemiBold: Raleway_600SemiBold,
-  RalewayBold: Raleway_700Bold,
-  RalewayExtraBold: Raleway_800ExtraBold,
-  RalewayBlack: Raleway_900Black
+  "ComfortaaLight": require('../fonts/Comfortaa/static/Comfortaa-Light.ttf'),
+  "Comfortaa_Regular": require('../fonts/Comfortaa/static/Comfortaa-Regular.ttf'),
+  "ComfortaaMedium": require('../fonts/Comfortaa/static/Comfortaa-Medium.ttf'),
+  "ComfortaaSemiBold": require('../fonts/Comfortaa/static/Comfortaa-SemiBold.ttf'),
+  "ComfortaaBold": require('../fonts/Comfortaa/static/Comfortaa-Bold.ttf'),
+  "RalewayThin": require('../fonts/Raleway/static/Raleway-Thin.ttf'),
+  "RalewayExtraLight": require('../fonts/Raleway/static/Raleway-ExtraLight.ttf'),
+  "RalewayLight": require('../fonts/Raleway/static/Raleway-Light.ttf'),
+  "RalewayRegular": require('../fonts/Raleway/static/Raleway-Regular.ttf'),
+  "RalewayMedium": require('../fonts/Raleway/static/Raleway-Medium.ttf'),
+  "RalewaySemiBold": require('../fonts/Raleway/static/Raleway-SemiBold.ttf'),
+  "RalewayBold": require('../fonts/Raleway/static/Raleway-Bold.ttf'),
+  "RalewayExtraBold": require('../fonts/Raleway/static/Raleway-ExtraBold.ttf'),
+  "RalewayBlack": require('../fonts/Raleway/static/Raleway-Black.ttf')
 };
 
-async function* promiseGenerator<T>(promise: Promise<T>): AsyncGenerator<T>{
-  yield await promise;
-}
+// async function* promiseGenerator<T>(promise: Promise<T>): AsyncGenerator<T>{
+//   yield await promise;
+// }
 
 export async function loadFonts() {
-  // const [fontsLoaded] =
-    return useFonts(fontNames);
-  // console.log(fontsLoaded)
-  // return promiseGenerator(Promise.resolve([fontsLoaded, null]))
-}
+  let [fontsLoaded, fontError] = useFonts(fontNames);
+  return [
+    fontsLoaded,
+    fontError
+  ]
+};
