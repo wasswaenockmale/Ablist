ce846c30d6f399fc2dc5beaa08ef417330d2a280
wasswaenockmale <enockmale97@gmail.com>
2023-11-16 11:25:43 +0300

    chore: defined types

---
+++ src/utils/types.ts
@@ -0,0 +1,40 @@
+import { RouteProp } from "@react-navigation/native"
+
+type NewsRouteParams = {
+  title: string
+  content: string
+  excerpt: string 
+  datePublised: string
+  featured_image: string
+}
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: {
+    title: string,
+    excerpt: string,
+    articleID: number,
+    publishedAt: string,
+    featured_image: string,
+    articleContent: string,
+  };
+  CodeTips: undefined;
+}
+
+type DetailsScreenProps = RouteProp<RootStackParamList, 'Details'>
+
+// interface for TalentSubmissionForm
+type TalentSubmissionForm = {
+  name: string
+  email: string
+  phone: string
+  company: string
+  lookingFor: string
+  message: string
+}
+export {
+  NewsRouteParams,
+  RootStackParamList,
+  DetailsScreenProps,
+  TalentSubmissionForm
+}
