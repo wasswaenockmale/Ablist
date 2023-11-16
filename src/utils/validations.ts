5b85287b4d3ec4f45793b2cbda0e9f9dac7d2e13
wasswaenockmale <enockmale97@gmail.com>
2023-11-16 11:26:35 +0300

    chore: validation for the forms submitted

---
+++ src/utils/validations.ts
@@ -0,0 +1,33 @@
+import * as Yup from 'yup';
+
+const TalentFormValidationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(1, 'Name too short')
+    .max(70, 'Name too long')
+    .required('Your name is required.'),
+  email: Yup.string()
+    .email('Invalid email')
+    .when('phone', {
+      is: (phone: string) => !phone || phone.trim() === '',
+      then: Yup.string().email("Email or phone is required"),
+      otherwise: Yup.string().email().notRequired(),
+    }),
+  phone: Yup.string()
+    .min(10, "Number entered is too short.")
+    .max(15, "Number entered is too long")
+    .when('email', {
+      is: (email: string) => !email || email.trim() === '',
+      then: Yup.string().required("Email or phone is required"),
+      otherwise: Yup.string().notRequired();
+    }),
+  company: Yup.string()
+    .required('Organization name is required'),
+  message: Yup.string(),
+  lookingFor: Yup.string()
+    .required('Select a service you are looking for.'),
+});
+
+
+export {
+  TalentFormValidationSchema
+}
